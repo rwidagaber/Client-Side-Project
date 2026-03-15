b9fe95643eef461d5cf9d045fdb36a6e306b79d4
rwida <rwidajaber@gmail.com>
2026-03-15 19:36:32 +0200

    Add products display

---
+++ SCRIPTs/product.js
@@ -0,0 +1,40 @@
+var xhr = new XMLHttpRequest()
+xhr.open('GET','/all_products.json');
+
+xhr.responseType = 'json'
+
+xhr.send()
+
+xhr.onload = function () {
+
+  var products = xhr.response.data
+  var cardContainer = document.getElementById('products')
+
+  for (var product of products) {
+
+    var card = document.createElement('div')
+    card.classList.add('card')
+
+    card.innerHTML = `
+        <div class="card-img">
+          <img src="${product.product_images[0]}" alt="${product.product_title}">
+          <div class="favorite">♡</div>
+        </div>
+
+        <div class="card-body">
+          <h3>${product.product_title}</h3>
+          <p class="category">${product.category}</p>
+          <div class="price">${product.product_price}</div>
+           <button class="cart-btn"
+              onclick="window.open('product_details.html?id=${product.id}','_blank')">
+              See Details
+            </button>
+           </div>
+    `
+
+    cardContainer.appendChild(card)
+
+  }
+
+}
+
