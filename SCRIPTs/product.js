a4998345561ae15d8cdf2efde0bbeb4243851e03
rwida <rwidajaber@gmail.com>
2026-03-16 12:03:33 +0200

    Refactor product display code

--- SCRIPTs/product.js
+++ SCRIPTs/product.js
@@ -1,40 +1,49 @@
 var xhr = new XMLHttpRequest()
-xhr.open('GET','/all_products.json');
+xhr.open('GET', '/all_products.json');
 
 xhr.responseType = 'json'
 
 xhr.send()
-
+var products
 xhr.onload = function () {
 
-  var products = xhr.response.data
+  products = xhr.response.data
+  displayProducts(products)
+
+}
+
+function displayProducts(products) {
+
   var cardContainer = document.getElementById('products')
+  cardContainer.innerHTML = ""
 
   for (var product of products) {
 
     var card = document.createElement('div')
     card.classList.add('card')
+    card.setAttribute('data-category', product.category)
 
     card.innerHTML = `
-        <div class="card-img">
-          <img src="${product.product_images[0]}" alt="${product.product_title}">
-          <div class="favorite">♡</div>
-        </div>
+      <div class="card-img">
+        <img src="${product.product_images[0]}" alt="${product.product_title}">
+        <div class="favorite"><i class="fi fi-rr-heart"></i></div>
+      </div>
 
-        <div class="card-body">
-          <h3>${product.product_title}</h3>
-          <p class="category">${product.category}</p>
-          <div class="price">${product.product_price}</div>
-           <button class="cart-btn"
-              onclick="window.open('product_details.html?id=${product.id}','_blank')">
-              See Details
-            </button>
-           </div>
+      <div class="card-body">
+        <h3>${product.product_title}</h3>
+        <p class="category">${product.category}</p>
+        <div class="price">${product.product_price}</div>
+
+        <button class="cart-btn"
+        onclick="window.open('product_details.html?id=${product.id}','_blank')">
+        See Details
+        </button>
+      </div>
     `
 
     cardContainer.appendChild(card)
-
   }
-
 }
+
+
 
